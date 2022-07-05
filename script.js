eaba7c43407fa8000d5ca733b3cc563b4f4f0ab0
ricozacharias <89130151+ricozacharias@users.noreply.github.com>
2022-07-05 13:39:24 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+import $ from 'jquery'
+    
+const colors = ["red", "green", "blue", "yellow", "orange"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+async function changeColor()
+{
+  let randColorIndex = getRandomInt(colors.length-1);
+  let color = colors[randColorIndex];
+  let textcolor = color !== "yellow" ? "white" : "black";
+
+  $('h1').text("...");
+  $('body').css('background-color', "black");
+  await delay(500);
+
+  $('h1').css('color', textcolor);
+  $('h1').text("Change color");
+  $('body').css('background-color', color);
+
+}
+
+// Main
+$('body').on('click', function() {
+  changeColor();
+});
